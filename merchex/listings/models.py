fd84d3a65b0211b92dbfb873d3cec92727c30605
Rod97139 <fayaflame@gmail.com>
2023-05-27 00:11:20 +0200

    update models

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,12 +1,35 @@
 from django.db import models
-
-# Create your models here.
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+    
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+    class Type(models.TextChoices):
+        RECORDS = 'Records'
+        CLOTHING = 'Clothing'
+        POSTERS = 'Posters'
+        MISCELLANEOUS = 'Miscellaneous'
+    
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    type = models.fields.CharField(choices=Type.choices, max_length=20)
 
 
 # >>> from listings.models import Listing
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -7,30 +7,20 @@ from listings.models import Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-""")
+    return render(request, 
+                  'listings/hello.html',
+                     {'bands': bands}
+                  )
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
 
 def listings(request):
     listings = Listing.objects.all()
-    return HttpResponse(f"""
-        <h1>Listings </h1>
-        <p>Annonces  :<p>
-        <ul>
-            <li>{listings[0].title}</li>
-            <li>{listings[1].title}</li>
-            <li>{listings[2].title}</li>
-        </ul>
-""")
+    return render(request, 
+                  'listings/listings.html',
+                     {'listings': listings}
+                  )
 
 def contact(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
